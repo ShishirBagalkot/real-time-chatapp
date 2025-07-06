643a9a3648f606af9d4feefce46142302f419bf2
Shishir Bagalkot <59408046+Shishirbagalkot@users.noreply.github.com>
2025-07-06 20:33:37 +0530

    Renamed main class and validated first run

--- src/main/java/com/shishir/real_time_chatapp/RealTimeChatApplication.java
+++ src/main/java/com/shishir/real_time_chatapp/RealTimeChatApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RealTimeChatappApplication {
+public class RealTimeChatApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RealTimeChatappApplication.class, args);
+		SpringApplication.run(RealTimeChatApplication.class, args);
 	}
-
 }
--- src/test/java/com/shishir/real_time_chatapp/RealTimeChatApplicationTests.java
+++ src/test/java/com/shishir/real_time_chatapp/RealTimeChatApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class RealTimeChatappApplicationTests {
+class RealTimeChatApplicationTests {
 
 	@Test
 	void contextLoads() {
