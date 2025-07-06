e427dfc1f200dcfb7ed8cf1c7b2330cb390e32d5
Shishir Bagalkot <59408046+Shishirbagalkot@users.noreply.github.com>
2025-07-06 20:17:06 +0530

    Initial commit

---
+++ src/main/java/com/shishir/real_time_chatapp/RealTimeChatappApplication.java
@@ -0,0 +1,13 @@
+package com.shishir.real_time_chatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTimeChatappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealTimeChatappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shishir/real_time_chatapp/RealTimeChatappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shishir.real_time_chatapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTimeChatappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
